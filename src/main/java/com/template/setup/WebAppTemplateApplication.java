c6c0d61edf9e7497cbb645fcf74136d85e03731c
mert1745 <mertkose1745@gmail.com>
2023-09-30 14:19:12 +0300

    Fix data configuration error

--- src/main/java/com/template/setup/WebAppTemplateApplication.java
+++ src/main/java/com/template/setup/WebAppTemplateApplication.java
@@ -2,9 +2,8 @@ package com.template.setup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class WebAppTemplateApplication {
 
     public static void main(String[] args) {
